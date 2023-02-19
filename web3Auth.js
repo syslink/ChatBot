77b00574ac5b3fcec8da37c1e97b0661374d507d
syslink <oursyslink@163.com>
2023-02-20 00:25:49 +0800

    fix

--- web3Auth.js
+++ web3Auth.js
@@ -1,5 +1,8 @@
 import Web3 from "web3";
-import vipABI from './vip.json' assert { type: "json" };;
+import * as dotenv from 'dotenv';
+import vipABI from './vip.json' assert { type: "json" };
+
+dotenv.config()
 
 const web3 = new Web3('https://nd-645-530-838.p2pify.com/181a9755ad317732b98d898de7107adf');
 const { privateKey, vipContractAddr } = process.env;
@@ -8,14 +11,14 @@ const vipContract = new web3.eth.Contract(vipABI, vipContractAddr);
 
 export function sign(userName, userAddr) {
     const telegramId = web3.utils.sha3(userName + '');
-    const messageHash =  web3.utils.sha3(telegramId + userAddr);
+    const messageHash = web3.utils.sha3(telegramId + userAddr);
     const signature = web3.eth.accounts.sign(messageHash, privateKey);
-    signature.telegramId = telegramId;
-    return signature;
+    const result = {telegramId, v: signature.v, s: signature.s, r: signature.r}
+    return result;
 }
 
 export async function checkVip(userName) {
-    const telegramId = web3.utils.sha3(userName);
+    const telegramId = web3.utils.sha3(userName + "");
     try {        
         let result = await vipContract.methods.telegramId2TokenIdMap(telegramId).call();
         console.log(result);
