8afac36a687b357b094f773b75a881c3e5b36273
syslink <oursyslink@163.com>
2023-03-08 23:13:50 +0800

    use azure speech

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const vip = new VIP();
 const openAI = new OpenAI(apiKey, gptModel, logger);
 
 const telegramBot = new TelegramChatBot(speakbot_token, mongodb, maxVoiceDialogNumber, true, vip, openAI, groupPrefix, logger, bInLocal == 1);
-const speech = new AWSSpeechWrapper(telegramBot, aws_access_key, aws_secret_key, mongodb, logger);
+const speech = new SpeechWrapper(telegramBot, SPEECH_KEY, SPEECH_REGION, mongodb, logger);
 telegramBot.startListen(speech);
 
 
